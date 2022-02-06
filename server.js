00fa00283d3e2dba703327f527701fb178e66819
13prashant <pacific1392@gmail.com>
2022-02-06 19:18:03 +0530

    created basic server & connected to db

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const colors = require("colors");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Initialize express app
+const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+});
